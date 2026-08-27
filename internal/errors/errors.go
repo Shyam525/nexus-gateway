5b310dcf4107ab88e3e1e6defa4fdb4a3babac99
shyam525 <makvanashyam21@gmail.com>
2026-08-27 16:12:21 +0530

    feat(errors): add structured application error types and error codes

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -1,4 +1,26 @@
+// package errors
+
+// // TODO: Implement errors
+
+
+
 package errors
 
-// TODO: Implement errors
+type Code string
 
+const (
+	InvalidRequest Code = "INVALID_REQUEST"
+	InternalError  Code = "INTERNAL_ERROR"
+)
+
+type Error struct {
+	Code    Code   `json:"code"`
+	Message string `json:"message"`
+}
+
+func New(code Code, message string) *Error {
+	return &Error{
+		Code:    code,
+		Message: message,
+	}
+}
